ff71b47e2a786e780df2ae7536dd51dc07514496
Vitor Schneider <vitorschneider6@hotmail.com>
2026-02-10 01:13:09 -0300

    feat: Product mapper

---
+++ src/main/java/com/vitor/gerenciadordeprodutos/Business/Mappers/ProductMapper.java
@@ -0,0 +1,28 @@
+package com.vitor.gerenciadordeprodutos.Business.Mappers;
+
+import com.vitor.gerenciadordeprodutos.Communication.DTOs.ProductDTO;
+import com.vitor.gerenciadordeprodutos.Communication.DTOs.RawMaterialDTO;
+import com.vitor.gerenciadordeprodutos.Domain.Models.ProductModel;
+import com.vitor.gerenciadordeprodutos.Domain.Models.RawMaterialModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+    public ProductModel map(ProductDTO dto){
+        ProductModel product = new ProductModel();
+        product.setName(dto.getName());
+        product.setDescription(dto.getDescription());
+        product.setActive(dto.getActive());
+
+        return product;
+    }
+
+    public ProductDTO map(ProductModel model){
+        ProductDTO dto = new ProductDTO();
+        dto.setName(model.getName());
+        dto.setDescription(model.getDescription());
+        dto.setActive(model.isActive());
+
+        return dto;
+    }
+}
